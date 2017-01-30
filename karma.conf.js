707de6ad89bc3d6e8778c8fa9c87a1af9e0cf971
MohanT <mohan.tadikimalla@fireeye.com>
2017-01-30 07:48:09 +0530

    Fix test run results

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,11 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-            'node_modules/angular/angular.js',
+            'node_modules/angular/angular.min.js',
             'node_modules/angular-mocks/angular-mocks.js',
+    		'node_modules/angular-animate/angular-animate.min.js',
+    		'node_modules/angular-resource/angular-resource.min.js',
+    		'node_modules/angular-sanitize/angular-sanitize.min.js',
             'static/*/*.js',
             'test/*/*.js'
     ],
--- test/controllers/controllersSpec.js
+++ test/controllers/controllersSpec.js
@@ -1,15 +1,20 @@
-/**
- * Testing controllers
- */
+/* testing filters */
+ 
+describe('capital first letter filter', function() {
+	beforeEach(module('utApp'));
+	var $filter;
+	  
+	beforeEach(inject(function(_$filter_){
+		$filter = _$filter_;
+	}));
 
-describe('example test', function() {
-	it('should be true', function() {
-		expect('foo').toBe('foo');
-		expect('kumar').toBe('kumar');
+	it('returns coverted string', function() {
+		var captial = $filter('capital');
+		expect(captial('pending')).toEqual('Pending');
 	});
 });
 
-
+/* testing controllers */
 describe('PasswordController', function() {
   beforeEach(module('utApp'));
 
@@ -42,52 +47,13 @@ describe('PasswordController', function() {
   });
 });
 
+/* testing directives */
 
-describe('length filter', function() {
-  beforeEach(module('utApp'));	
-  var $filter;
 
-  beforeEach(inject(function(_$filter_){
-    $filter = _$filter_;
-  }));
 
-  it('returns 0 when given null', function() {
-    var length = $filter('length');
-    expect(length(null)).toEqual(0);
-  });
 
-  it('returns the correct value when given a string of chars', function() {
-    var length = $filter('length');
-    expect(length('abc')).toEqual(3);
-  });
-});
 
-describe('Unit testing great quotes', function() {
-  var $compile,
-      $rootScope;
 
-  // Load the myApp module, which contains the directive
-  beforeEach(module('utApp'));
-
-  // Store references to $rootScope and $compile
-  // so they are available to all tests in this describe block
-  beforeEach(inject(function(_$compile_, _$rootScope_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-  }));
-  
-  it('Replaces the element with the appropriate content', function() {
-    // Compile a piece of HTML containing the directive
-    var compileLinkFn = $compile("<a-great-eye></a-great-eye>");
-    var element = compileLinkFn($rootScope);
-    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
-    $rootScope.$digest();
-    // Check that the compiled element contains the templated content
-    expect(element.html()).toContain("lidless, wreathed in flame, 2 times");
-  });
-  
-});
 
 
 
